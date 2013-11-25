30ac30ce809b4f903fbc7f9c98c1fd75fb237d1e
WalrusCow <willgmcd@gmail.com>
2013-11-24 22:44:03 -0600

    A few points utils

--- sg.js
+++ sg.js
@@ -110,5 +110,16 @@ define(function() {
 
   };
 
+  sg.copyPoint = function(pt) {
+    return { x: pt.x, y: pt.y };
+  }
+
+  sg.addPoints = function(pt1, pt2) {
+    return {
+      x: pt1.x + pt2.x,
+      y: pt1.y + pt2.y
+    };
+  }
+
   return sg;
 });
